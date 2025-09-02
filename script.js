b1d16cd0daa986c03b8c73c42dfe83efb73ea8f4
t-presa <159184713+t-presa@users.noreply.github.com>
2025-09-02 11:16:03 -0300

    trabalho dia 02 de setembro de 2025

---
+++ script.js
@@ -0,0 +1,31 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado= document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultadol");
+
+const perguntas = {
+enunciado:"pergunta 1", 
+alternativas[
+"Alternativa 1",
+"Alternativa 2",
+
+]
+
+}
+
+{
+    enunciado:"pergunta 2", 
+    alternativas[
+    "Alternativa 1",
+    "Alternativa 2",
+    ]
+}
+ {
+    enunciado:"pergunta 3", 
+    alternativas[
+    "Alternativa 1",
+    "Alternativa 2",
+    ]
+ }
+
