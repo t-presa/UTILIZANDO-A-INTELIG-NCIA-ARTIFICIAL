896c6b30791c8b46273d6175b64acb74eb4996c5
t-presa <159184713+t-presa@users.noreply.github.com>
2025-09-24 11:16:34 -0300

    trabalho dia 24 de Setembro de 2025

--- script.js
+++ script.js
@@ -6,54 +6,54 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Assim que saiu da escola você se depara com uma nova pergunta em relação a sua vida, qual vestibular fazer?",
         alternativas: [
             {
                 texto: "Isso é assustador!",
                 afirmacao: "No início ficou com medo do que os vestibulares podem fazer em minha vida. "
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "Isso é dificil!",
                 afirmacao: "Quis saber como lidar com os vestibulares no dia a dia."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Uma vestibulanda decidiu fazer uma sequência de vestibulares.",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Utiliza uma ferramenta de busca na internet que ajude estudantes na escolha do vestibular ideal.",
+                afirmacao: "Conseguiu utilizar sites para buscar informações úteis."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Fez os vestibulares com base no que estudou durante todo o percursso de sua vida.",
+                afirmacao: "Sentiu mais facilidade em fazer as provas devido ao seu conhecimento."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Após a realização do vestibular se sentiu bastante confiante por ter estudado e conseguido resolver as questões.",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Defende a ideia de que se esforçar realmente vale a pena.",
+                afirmacao: "Vem impulsionando a novos estudantes irem em busca de seus sonhos."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Me preocupo com as pessoas que perderão oportunidades por conta da pressão psicológica.",
+                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre estudantes para discutir quais são suas preucupações."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Ao final do períldo escolar você precisa escolher o seu futuro, e agora?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "seguir a profissão que eu queo para minha vida.",
+                afirmacao: "Notou também que muitas pessoas ainda não sabem oque seguir de profissão no futuro."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "seguir a profissão que as pessoas querem que eu faça.",
+                afirmacao: "muitas pessoas sentem dificuldades de fazer escolhas em relação a vida profissional por conta das pessoas ao seu redor também!"
             }
         ]
     },
